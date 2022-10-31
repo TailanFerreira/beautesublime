a152b885e19f37e5506aa779f8a43ce33b0d26f5
Tailan Ferreira <tailan.frreira44@gmail.com>
2022-10-31 19:30:23 -0300

    Novas atualizações

--- app/src/main/java/com/example/trabalhofinaltailan/LoginActivity.java
+++ app/src/main/java/com/example/trabalhofinaltailan/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.trabalhofinaltailan;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +21,8 @@ public class LoginActivity extends AppCompatActivity {
     btnentrar.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            setContentView(R.layout.activity_main);
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
         }
     });
 
--- app/src/main/java/com/example/trabalhofinaltailan/MainActivity.java
+++ app/src/main/java/com/example/trabalhofinaltailan/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.trabalhofinaltailan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,7 +40,8 @@ private Button btn_clientes;
         btn_clientes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.activity_cliente);
+                Intent intent = new Intent(getApplicationContext(), Clientes.class);
+                startActivity(intent);
             }
         });
     }
